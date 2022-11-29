f85441f92314a69d6304eae6e9db2644c6290a66
Achmad Alfiansyah <achmadalfiansyah069@gmail.com>
2022-11-29 22:54:04 +0700

    setup view

---
+++ app/src/main/java/com/alfiansyah/notesapp/AddNoteActivity.kt
@@ -0,0 +1,11 @@
+package com.alfiansyah.notesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
