698cb14697da8c377cdb68afd88acaddaca78e38
Achmad Alfiansyah <achmadalfiansyah069@gmail.com>
2022-12-06 23:23:22 +0700

    setup UI

--- app/src/main/java/com/alfiansyah/notesapp/ui/fragments/CreateNotesFragment.kt
+++ app/src/main/java/com/alfiansyah/notesapp/ui/fragments/CreateNotesFragment.kt
@@ -1,20 +1,24 @@
 package com.alfiansyah.notesapp.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.alfiansyah.notesapp.R
+import androidx.fragment.app.Fragment
+import com.alfiansyah.notesapp.databinding.FragmentCreateNotesBinding
 
 class CreateNotesFragment : Fragment() {
+    private var _binding:FragmentCreateNotesBinding?=null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_notes, container, false)
+        _binding = FragmentCreateNotesBinding.inflate(layoutInflater,container,false)
+
+        return binding.root
     }
 
 }
