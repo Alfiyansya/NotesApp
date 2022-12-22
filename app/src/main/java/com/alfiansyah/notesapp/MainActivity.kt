a96a4ebd353fcd5642dfc190e8d726e35eba8da4
Achmad Alfiansyah <achmadalfiansyah069@gmail.com>
2022-12-22 22:06:09 +0700

    Fixing bug when open the Apps

--- app/src/main/java/com/alfiansyah/notesapp/MainActivity.kt
+++ app/src/main/java/com/alfiansyah/notesapp/MainActivity.kt
@@ -1,21 +1,11 @@
 package com.alfiansyah.notesapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navController = findNavController(R.id.fragmentContainerView)
-        setupActionBarWithNavController(navController)
-    }
-
-    override fun onNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onNavigateUp()
     }
 }
